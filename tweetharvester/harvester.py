5fa5f7e898431c844baa26c178c775dc9d9bbd1a
dteh <d.we.teh@gmail.com>
2015-04-26 12:56:07 +1000

    Working streaming test for Melbourne CBD

--- tweetharvester/harvester.py
+++ tweetharvester/harvester.py
@@ -3,16 +3,18 @@
 import tweepy, json
 
 ## OAuth Keys
+# Application Key
 consumer_key = "pSFHu1r2xAbqtbOsVvOU5vuFP"
 consumer_key_secret = "hjMn6oCPCyBUanidBwAImXX051ILf1JVhrW4IO268qZS00IFvI"
+# Daniel's personal twitter account key (DON'T GET ME BANNED PLEASE)
 access_token = "19472785-vQJRKwqBNuOMs1s5AwTzv3wFWQlqtjeIflj5rNuyy"
 access_token_secret = "R88rhzJBF3Rr85HDHn5kMKRkSmOEGKLJAO4mDrtK2ic7C"
 
-## Something
+## Init OAuth
 auth = tweepy.OAuthHandler(consumer_key, consumer_key_secret)
 auth.set_access_token(access_token, access_token_secret)
 
-##
+## Main
 api = tweepy.API(auth)
 print "Interface set up.."
 
@@ -21,7 +23,6 @@ class StdOutListener(tweepy.StreamListener):
         decoded = json.loads(data)
         
         print '@%s: %s' %(decoded['user']['screen_name'], decoded['text'].encode('ascii', 'ignore'))
-#        print data
         print ""
         return True
     def on_error(self,status):
@@ -32,10 +33,8 @@ stream = tweepy.Stream(auth,l)
 print "*"*78
 print "Getting tweets from Melbourne CBD.."
 print "*"*78
-#stream.filter(locations=[-79.72452163696289,43.37158360778362,-79.72297668457031,43.527517 7007501])
-stream.filter(locations=[-37.847579,144.923850,-37.785335,145.001785])
 
-#public_tweets = api.home_timeline()
-#for tweet in public_tweets: 
-#    print tweet.text
+# use bbox.com to figure out bounding boxes lat/long coordinates
+# DO NOT USE GOOGLE MAPS! (coordinates are flipped)
+stream.filter(locations=[144.909496,-37.851137,145.001163,-37.785639])
     
