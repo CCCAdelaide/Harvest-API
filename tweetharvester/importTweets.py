e5f7c0e77731eef7eead09ba75d7e6083d712fd5
dteh <d.we.teh@gmail.com>
2015-05-10 18:10:29 +1000

    more logging

--- tweetharvester/importTweets.py
+++ tweetharvester/importTweets.py
@@ -6,11 +6,14 @@ filename = 'adelaide.csv'
 storage = TweetStore('tweets_adelaide')
 
 i = 0
-with open(filename, 'rb') as tweetfile
+with open(filename, 'rb') as tweetfile:
     reader = csv.DictReader(tweetfile)
-    for tweet in reader
+    for tweet in reader:
         decoded = json.loads(tweet['value'])
-        storage.save_tweet(decoded)
+        try:
+            storage.save_tweet(decoded)
+        except:
+            print("Tweet %d already exists.. skipping" % i)
         print("Importing tweet no. %d" % i)
         i+=1
 print ("Inserted %d tweets" % i)
